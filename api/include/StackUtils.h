8e76aafc00a881d51ca816c46cad1edc2cff39be
Brahvim <69293652+Brahvim@users.noreply.github.com>
2024-04-27 16:27:22 +0530

    ANOTHER bug covered!

--- api/include/StackUtils.h
+++ api/include/StackUtils.h
@@ -1,5 +1,5 @@
-#define STACK_ERROR(x) x != STACK_OK
-#define STACK_NO_ERROR(x) x == STACK_OK
+#define STACK_ERROR(x) (x != STACK_OK)
+#define STACK_NO_ERROR(x) (x == STACK_OK)
 
 #define MAKE_STACK(type, name, initial_allocation_size, error_callback_code)    \
 	struct type##_stack *name;                                                  \
