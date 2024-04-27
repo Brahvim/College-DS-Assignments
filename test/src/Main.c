c0f62efc00cb6a1098da04b653bab6fc44bc1846
Brahvim <69293652+Brahvim@users.noreply.github.com>
2024-04-27 16:56:24 +0530

    Bugzilla LOL.

--- test/src/Main.c
+++ test/src/Main.c
@@ -1,20 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "Main.h"
 
 int main() {
-	char *name = "Brahvim";
+	char *name = calloc(8, sizeof(char));
+	strncpy(name, "Brahvim", 8);
+
 	MAKE_STACK(char, stack, sizeof(name), {
 		puts("Failed to create the stack.\n");
 		return EXIT_FAILURE;
 	});
 
+	char *reversed = calloc(8, sizeof(char));
+
 	string_to_stack(name, stack);
 	reverse_char_stack(stack);
-	stack_to_string(name, stack);
+	stack_to_string(stack, reversed);
 
-	puts(name);
+	puts("Reversed string:");
+
+	for (size_t i = 7; i > 0; --i)
+		putc(reversed[i], stdout);
+
+	free(reversed);
 
 	// Destroy the stack when done:
 	char_stack_destroy(stack);
@@ -30,11 +40,15 @@ void reverse_char_stack(struct char_stack *p_stack) {
 }
 
 void string_to_stack(char *p_string, struct char_stack *p_stack) {
-	for (int i = 0; i < p_stack->fits; i++)
+	for (int i = 0; i < p_stack->fits; ++i)
 		char_stack_push(p_stack, p_string[i]);
 }
 
 void stack_to_string(struct char_stack *p_stack, char *p_string) {
+
+
+
+
 	char c;
 	int counter = 0;
 	while (STACK_NO_ERROR(char_stack_poll(p_stack, &c)))
