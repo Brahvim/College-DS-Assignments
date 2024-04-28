d24617aa0b053d64a03e263256a95bcda316f000
Brahvim <69293652+Brahvim@users.noreply.github.com>
2024-04-28 23:09:13 +0530

    Merge changed too much. Bak we arrr.

--- test/src/Main.c
+++ test/src/Main.c
@@ -1,43 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 #include "Main.h"
 
-#define ORIGINAL_STRING ((char*)"Brahvim")
-#define ORIGINAL_STRING_SIZE (sizeof(ORIGINAL_STRING) * sizeof(char))
-#define ORIGINAL_STRING_LENGTH (sizeof(ORIGINAL_STRING) / sizeof(char))
-
 int main() {
-	MAKE_STACK_HANDLING_ALL(char, stack, ORIGINAL_STRING_SIZE, {
-		puts("Failed to allocate for `stack`.\n");
+	// Create a stack with an initial allocation size for `10` `int`s:
+	MAKE_STACK_HANDLING_ALL(int, stack, 10, {
+		puts("Failed to create the stack.\n");
 		return EXIT_FAILURE;
 	});
 
-	string_to_stack(ORIGINAL_STRING, stack);
+	// Push 15,000 elements onto the stack (a lot!):
+	for (int i = 1; i <= 15; ++i) {
+		if (STACK_ERROR(int_stack_push(stack, i)))
+			printf("Failed to push `%d` onto the stack.\n", i);
+		else
+			printf("Pushed `%d` onto the stack.\n", i);
+	}
 
-	char *reversed_string = malloc(ORIGINAL_STRING_SIZE);
-	stack_to_string(stack, reversed_string);
-	printf("Reversed string: \"%s\"\n", reversed_string);
+	// Pop elements from the stack until it's empty:
+	{
+		int i;
+		while (STACK_NO_ERROR(int_stack_poll(stack, &i)))
+			printf("Popped `%d` from the stack.\n", i);
+	}
 
-	// No leaks here!:
-	free(reversed_string);
-	char_stack_destroy(stack);
-}
-
-void string_to_stack(const char *p_string, struct char_stack *p_stack) {
-	for (int i = 0; i < p_stack->fits; ++i)
-		char_stack_push(p_stack, p_string[i]);
-}
-
-void stack_to_string(struct char_stack *p_stack, char *p_string) {
-	char c;
-	stack_status s;
-	int counter = 0;
-	char_stack_pop(p_stack); // C-strings, well, they have a `\0` at the end...
-
-	while (STACK_NO_ERROR(s = char_stack_poll(p_stack, &c)))
-		p_string[counter++] = c;
-
-	p_string[counter] = '\0';
+	// Destroy the stack when done:
+	int_stack_destroy(stack);
 }
