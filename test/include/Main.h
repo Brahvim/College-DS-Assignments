7cca6dda042c9ff4f3ba740e4c1dae37ff540457
Brahvim <69293652+Brahvim@users.noreply.github.com>
2024-04-28 23:28:31 +0530

    Initial commit! Stuff incoming!

--- test/include/Main.h
+++ test/include/Main.h
@@ -1,8 +1,2 @@
 #pragma once
 #include "Stack.h"
-
-MAKE_STACK_STRUCT(char);
-MAKE_STACK_METHODS(char);
-
-void string_to_stack(const char *string, struct char_stack *stack);
-void stack_to_string(struct char_stack *stack, char *string);
