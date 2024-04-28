30c8bc8f35a3424e9418a2ca4c57540768a66087
Brahvim <69293652+Brahvim@users.noreply.github.com>
2024-04-28 16:13:19 +0530

    Fixed a memory leak in the macro!

--- api/include/StackUtils.h
+++ api/include/StackUtils.h
@@ -1,8 +1,8 @@
 #define STACK_ERROR(x) (x != STACK_OK)
 #define STACK_NO_ERROR(x) (x == STACK_OK)
 
-#define MAKE_STACK_HANDLING_ALL(type, name, initial_allocation_size, error_code_block)					\
-	MAKE_STACK_HANDLING_EACH(type, name, initial_allocation_size, error_code_block, error_code_block)
+#define MAKE_STACK_HANDLING_ALL(type, name, initial_allocation_size, error_code_block)									\
+	MAKE_STACK_HANDLING_EACH(type, name, initial_allocation_size, error_code_block, { free(name); } error_code_block)
 
 #define MAKE_STACK_HANDLING_EACH(type, name, initial_allocation_size, malloc_code_block, back_malloc_code_block)	\
 	struct type##_stack *name = NULL;                                       										\
