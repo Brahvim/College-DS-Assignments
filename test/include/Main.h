e4a238aead857ccde810fff9eecd9d9e08622348
Brahvim <69293652+Brahvim@users.noreply.github.com>
2024-04-29 08:02:20 +0530

    MUST NOT FORGET THOSE CREDITS!

--- test/include/Main.h
+++ test/include/Main.h
@@ -11,8 +11,14 @@ typedef enum {
 
 MAKE_STACK_OF(char);
 
+/**
+ * [ https://stackoverflow.com/questions/52984551/using-fgets-with-realloc ],
+ * [ https://github.com/Brahvim/College-C-Assignments/blob/26934f9777473ebb2a759f2f17f9715820ffa46f/ReadLine.c#L7 ].
+ */
 void clear_stdin(void);
 char* read_line(const size_t factor);
+
+// From the previous question, <chuckle>!:
 void stack_to_string(struct char_stack *stack, char *string);
 void string_to_stack(const char *string, struct char_stack *stack);
 
