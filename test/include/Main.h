bb9e511dda641bc94e6573e327e4090775c39198
Brahvim <69293652+Brahvim@users.noreply.github.com>
2024-05-05 07:47:44 +0530

    Making this more generic...

--- test/include/Main.h
+++ test/include/Main.h
@@ -4,22 +4,31 @@
 #include "ReadLine.h"
 
 typedef enum {
-    POSTFIX_ADD = '+',
-    POSTFIX_DIVIDE = '/',
-    // POSTFIX_MODULO = '%',
-    POSTFIX_MULTIPLY = '*',
-    POSTFIX_SUBTRACT = '-',
-} postfix_operation;
+    POSTFIX_OPERATION_ADD = '+',
+    POSTFIX_OPERATION_DIVIDE = '/',
+    // POSTFIX_OPERATION_MODULO = '%',
+    POSTFIX_OPERATION_MULTIPLY = '*',
+    POSTFIX_OPERATION_SUBTRACT = '-',
+} postfix_evaluator_operation;
 
-const int g_postfix_operations[4] = {
-    POSTFIX_ADD,
-    POSTFIX_DIVIDE,
-    POSTFIX_MULTIPLY,
-    POSTFIX_SUBTRACT,
+const int g_postfix_evaluator_operations[4] = {
+    POSTFIX_OPERATION_ADD,
+    POSTFIX_OPERATION_DIVIDE,
+    POSTFIX_OPERATION_MULTIPLY,
+    POSTFIX_OPERATION_SUBTRACT,
+};
+
+typedef enum {
+    POSTFIX_ERROR_A,
+} postfix_evaluator_error;
+
+const int g_postfix_evaluator_errors[4] = {
+    POSTFIX_ERROR_A,
 };
 
 MAKE_STACK_OF(double);
 typedef char expr_char_t;
 
 // Program-specific stuff:
-void postfix_evaluator_error(const size_t p_error_char_id, const char *p_error_message);
+double postfix_evaluator_evaluate(expr_char_t *expr);
+void postfix_evaluator_report_error(const size_t error_char_id, const char *error_message);
--- test/src/Main.c
+++ test/src/Main.c
@@ -10,7 +10,7 @@ int main() {
 	puts("Welcome to the literals postfix expression evaluator.");
 	puts("Supported operators include:");
 	for (size_t i = 0; i < 4; i++)
-		printf("%zu. `%c`.\n", i, g_postfix_operations[i]);
+		printf("%zu. `%c`.\n", i, g_postfix_evaluator_operations[i]);
 
 	puts("Please enter your postfix expression below:");
 
@@ -18,16 +18,36 @@ int main() {
 	expr_char_t *expr = read_line(5, &expr_len);
 	++expr_len;
 
+	// double result = 0;
+	// switch (false) {
+	// 	case true: {
+	//
+	// 	} break;
+	//
+	// 	case false: {
+	//
+	// 	} break;
+	//
+	// 	default: {
+	//
+	// 	} break;
+	// }
+
+	postfix_evaluator_evaluate(expr);
+	free(expr);
+}
+
+#pragma region // Program-specific stuff.
+double postfix_evaluator_evaluate(expr_char_t *p_expr) {
 	MAKE_STACK_HANDLING_ALL(double, operand_stack, 0, {
 		puts("Failed to allocate for `operand_stack`...\n");
 		exit(EXIT_FAILURE);
 	});
 
 	expr_char_t c = 0;
-	// double result = 0;
 	size_t expr_chars_processed = 0;
 
-	for (expr_char_t *expr_char_ptr = expr; (c = *expr_char_ptr) != '\0'; expr_char_ptr += sizeof(expr_char_t)) {
+	for (expr_char_t *expr_char_ptr = p_expr; (c = *expr_char_ptr) != '\0'; expr_char_ptr += sizeof(expr_char_t)) {
 		// Absolutely ignore whitespaces:
 		if (c == ' ' || c == '\t')
 			continue;
@@ -38,19 +58,8 @@ int main() {
 		if (ptr_to_char_after_num != expr_char_ptr) {
 			double_stack_push(operand_stack, num);
 		} else { // ...We may have some operand.
-			if (operand_stack->top < 2) { // Error!
-				puts("Hey! A postfix expression requires at least two operands (numbers) before an operator!");
-				puts("Your expression may be erroneous.");
-				printf("%s\n", expr_char_ptr);
-
-				// Tell them where the issue in their expression is!
-				for (size_t i = -1; i < expr_chars_processed; i++)
-					putc(' ', stdout);
-
-				puts("^");
-				puts("(Erroneous operator).");
-				exit(EXIT_FAILURE);
-			}
+			if (operand_stack->top < 2)  // Error!
+				postfix_evaluator_report_error(expr_chars_processed + 2, "(Two operands required for an operation.)");
 
 			double result = 0;
 			double n1 = 0, n2 = 0;
@@ -58,29 +67,29 @@ int main() {
 			double_stack_poll(operand_stack, &n2);
 
 			switch (c) {
-				case POSTFIX_ADD: {
+				case POSTFIX_OPERATION_ADD: {
 					result = n1 + n2;
 				} break;
 
-				case POSTFIX_DIVIDE: {
-					if (n1 == 0 || n2 == 0) {
-					}
+				case POSTFIX_OPERATION_DIVIDE: {
+					if (n1 == 0 || n2 == 0)
+						postfix_evaluator_report_error(expr_chars_processed + 2, "Division by zero!");
 
 					result = n1 / n2;
 				} break;
 
-								   //    case POSTFIX_MODULO: { // Kinda' don't wanna allow this one.
-								   // 		signed long long int n1_rounded = round(n1);
-								   // 		if (n1_rounded == n1)
-								   // 			result = (n1 % n2);
-								   //    } break;
+											 //    case POSTFIX_MODULO: { // Kinda' don't wanna allow this one.
+											 // 		signed long long int n1_rounded = round(n1);
+											 // 		if (n1_rounded == n1)
+											 // 			result = (n1 % n2);
+											 //    } break;
 
-				case POSTFIX_MULTIPLY: {
+				case POSTFIX_OPERATION_MULTIPLY: {
 					result = n1 * n2;
 				} break;
 
-				case POSTFIX_SUBTRACT: {
-					result = n1 - n2;
+				case POSTFIX_OPERATION_SUBTRACT: {
+					result = n2 - n1;
 				} break;
 
 				default: {
@@ -95,23 +104,22 @@ int main() {
 		++expr_chars_processed;
 	}
 
-	double result;
-	double_stack_poll(operand_stack, &result);
-
-	free(expr);
-	printf("Result: `%lf`.\n", result);
+	double to_ret = 0;
+	double_stack_poll(operand_stack, &to_ret);
 	double_stack_destroy(operand_stack);
+	return to_ret;
 }
 
-#pragma region // Program-specific stuff.
-void postfix_evaluator_error(const size_t p_error_char_id, const char *p_error_message) {
+void postfix_evaluator_report_error(const size_t p_error_char_id, const char *p_error_message) {
 	// printf("%s\n", expr);
+
 	// Where is the issue in the expression...?:
 	for (size_t i = 0; i < p_error_char_id; i++)
 		putc(' ', stdout);
 
 	puts("^"); // Better than two `putc()` calls!
 	puts(p_error_message);
+	puts("Please retry.");
 	exit(EXIT_FAILURE);
 }
 #pragma endregion
