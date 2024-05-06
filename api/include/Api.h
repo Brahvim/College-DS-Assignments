74556e7f9f336cd3c108457901dc461c1cf3fd6c
Brahvim <69293652+Brahvim@users.noreply.github.com>
2024-05-06 15:29:55 +0530

    Cleaner template.

--- api/include/Api.h
+++ api/include/Api.h
@@ -1,5 +1,1 @@
-// #ifndef MAKE_STACK_TYPE
-// #error "Please define `MAKE_STACK_TYPE` as the data type you want to use."
-// #else
-
-// #endif
+#pragma once
--- api/src/Api.c
+++ api/src/Api.c
@@ -1,4 +1,1 @@
-// #include "Stack.h"
-
-int main() {
-}
+#include "Api.h"
