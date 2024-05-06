e0af5b6c50ad28dc9d4b06e5900988947b9094e1
Brahvim <69293652+Brahvim@users.noreply.github.com>
2024-05-06 16:01:49 +0530

    Complete, I think.

--- test/include/Main.h
+++ test/include/Main.h
@@ -1,3 +1,8 @@
 #pragma once
 
+#include <stdio.h>
+#include <stdint.h>
+
 #include "Api.h"
+
+uint64_t factorial(uint64_t number);
--- test/src/Main.c
+++ test/src/Main.c
@@ -1,4 +1,31 @@
 #include "Main.h"
 
 int main() {
+	fputs("Put a number, get a factorial: ", stdout);
+
+	uint64_t input_or_result = 0;
+	scanf("%lu", &input_or_result);
+
+	// This can be improved further by comparing against what cannot be computed on an amd64 processor.
+	// That's `65`. ...and for `uint_32_t`, `12`!
+	input_or_result = factorial(input_or_result);
+
+	if (input_or_result == 0)
+		puts("That would be impossible to calculate (I tried!).");
+	else
+		printf("That would be `%lu`!\n", input_or_result);
+}
+
+// Forget tail-recursive optimizations; we need to make sure we don't overflow!:
+uint64_t factorial(uint64_t p_number) {
+	if (p_number < 2)
+		return p_number;
+
+	// Recursive call:
+	const uint64_t to_ret = p_number-- * factorial(p_number); // NOLINT(clang-diagnostic-unsequenced)
+
+	if (to_ret < p_number)
+		return 0;
+
+	return to_ret;
 }
