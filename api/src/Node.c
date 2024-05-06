4c23ff750675a9d64b49f583bbdea6cd6e6ef539
Brahvim <69293652+Brahvim@users.noreply.github.com>
2024-05-06 19:53:24 +0530

    This is clunk! Wait.

--- api/src/Node.c
+++ api/src/Node.c
@@ -16,22 +16,30 @@ enum node_status create_node(struct singly_linked_node **p_node) {
 
 enum node_status destroy_node(struct singly_linked_node *p_previous, struct singly_linked_node *p_to_free) {
     // ...It's an assignment requirement to use singly-linked nodes, LOL:
-    if (!(p_previous == NULL || p_to_free == NULL))
+    if (p_previous && p_to_free)
         p_previous->next = p_to_free->next;
 
     free(p_to_free);
 }
 
 enum node_status destroy_linked_list(struct singly_linked_node *p_first_node) {
-    if (p_first_node == NULL)
+    if (!p_first_node)
         return NODE_STATUS_NULL;
 
-    struct singly_linked_node *previous = p_first_node;
-    struct singly_linked_node *current = p_first_node->next;
+    struct singly_linked_node *current = p_first_node;
+    struct singly_linked_node *next = p_first_node->next;
 
-    while (current != NULL) {
-        if (current->next != NULL)
-            destroy_node(previous, current);
+    // Grab the current node,
+    while (current) {
+        free(current); // ...free it!
+
+        if (!next)
+            break;
+
+        current = next; // Copy the next one to be here. Doesn't matter if it's `NULL`.
+        // Now, `current` and `next` are the same!
+        // ...So, it literally doesn't matter which one's `next` we grab:
+        next = next->next;
     }
 
     return NODE_STATUS_NO_ERROR;
