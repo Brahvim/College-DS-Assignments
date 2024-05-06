2605587b61f3cc2e0927023936cfc451e3ebcc5f
Brahvim <69293652+Brahvim@users.noreply.github.com>
2024-05-06 19:35:37 +0530

    Formatting corrections.

--- api/include/Node.h
+++ api/include/Node.h
@@ -7,14 +7,14 @@
 #define NODE_ERROR(x) ((x) != NODE_STATUS_NO_ERROR)
 
 #define MAKE_NODE_HANDLING_ALL(name, error_name, error_code_block)		\
-struct singly_linked_node *name;                      								\
+struct singly_linked_node *name;                      					\
 enum node_status error_name;                      						\
 																		\
 if (NODE_ERROR(error_name = create_node(&name)))     					\
 	error_code_block													\
 
 #define MAKE_NODE_HANDLING_EACH(name, error_name, malloc_code_block, null_code_block)		\
-struct singly_linked_node *name;                      													\
+struct singly_linked_node *name;                      										\
 																							\
 switch (create_node(&name)) {																\
 	case NODE_STATUS_NULL:																	\
@@ -31,9 +31,9 @@ switch (create_node(&name)) {																\
 
 #define MAKE_LINKED_LIST_HANDLING_ALL(name, error_name, length, error_code_block)								\
 																												\
-	struct singly_linked_node *name = NULL;																					\
+	struct singly_linked_node *name = NULL;																		\
 	size_t name##_length = length;																				\
-	struct singly_linked_node *name_##_last_node = NULL;																		\
+	struct singly_linked_node *name_##_last_node = NULL;														\
 																												\
 	for (size_t name##_allocated_length = 0; name##_allocated_length < length; ++name##_allocated_length) {		\
 		enum node_status error_name;																			\
