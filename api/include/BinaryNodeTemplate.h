edfc695438de958699501f8fe7c1e5b25d390a5c
Brahvim <69293652+Brahvim@users.noreply.github.com>
2024-05-20 22:22:10 +0530

    Fixing backslash indent convention ***forever***.
    Assuming no backslashes have been written out, put the first one at the end of the longest line, and tab it thrice.
    
    Also, a line with only a backslash must be left at the end.

--- api/include/BinaryNodeTemplate.h
+++ api/include/BinaryNodeTemplate.h
@@ -4,60 +4,38 @@
 #include <memory.h>
 
 #define DECLARE_BINARY_NODE_STRUCT(type)			\
-struct binary_##type##_node {					\
-														\
-	type data;											\
-	struct binary_##type##_node *next;			\
-														\
-};														\
+struct binary_##type##_node {						\
+													\
+	type data;										\
+	struct binary_##type##_node *left;				\
+	struct binary_##type##_node *right;				\
+													\
+};													\
+													\
 
-#define DECLARE_BINARY_NODE_METHODS(type)																						\
-enum binary_node_status create_binary_##type##_node(struct binary_##type##_node **node);							\
-																																		\
-enum binary_node_status destroy_binary__##type##_list(struct binary_##type##_node *first_node);						\
-																																		\
-enum binary_node_status destroy_binary_##type##_node(																		\
-	struct binary_##type##_node *previous, 																						\
-	struct binary_##type##_node *to_destroy);																					\
+#define DECLARE_BINARY_NODE_METHODS(type)																\
+enum binary_node_status create_binary_##type##_node(struct binary_##type##_node **node);				\
+																										\
+enum binary_node_status destroy_binary_##type##_tree(struct binary_##type##_node *first_node);			\
+																										\
 
-#define DEFINE_BINARY_NODE_METHODS(type)																								\
-enum binary_node_status create_binary_##type##_node(struct binary_##type##_node **p_node) {							\
-	*p_node = malloc(sizeof(struct binary_##type##_node));																			\
-	struct binary_##type##_node *const node = *p_node;																				\
-																																			\
-	if (!node)																																\
-		return BINARY_NODE_STATUS_MALLOC;																											\
-																																			\
-	memset(&(node->data), 0, sizeof(type)); /* NOLINT(clang-analyzer-security.insecureAPI.DeprecatedOrUnsafeBufferHandling) */				\
-	node->next = NULL;																														\
-																																			\
-	return BINARY_NODE_STATUS_NO_ERROR;																											\
-}																																			\
-																																			\
-enum binary_node_status destroy_binary__##type##_list(struct binary_##type##_node *p_first_node) {						\
-	if (!p_first_node)																														\
-		return BINARY_NODE_STATUS_NULL;																											\
-																																			\
-	struct binary_##type##_node *current = p_first_node;																				\
-																																			\
-	while (current) {																														\
-		struct binary_##type##_node *const next = current->next;																		\
-		free(current);																														\
-		current = next;																														\
-	}																																		\
-																																			\
-	return BINARY_NODE_STATUS_NO_ERROR;																											\
-}																																			\
-																																			\
-enum binary_node_status destroy_binary_##type##_node(																			\
-	struct binary_##type##_node *p_previous, struct binary_##type##_node *p_to_free) {										\
-	/* ...It's an assignment requirement to use singly-linked nodes, LOL:*/																	\
-	if (p_previous && p_to_free)																											\
-		p_previous->next = p_to_free->next;																									\
-	else return BINARY_NODE_STATUS_NULL;																											\
-																																			\
-	free(p_to_free);																														\
-	return BINARY_NODE_STATUS_NULL;																												\
-}																																			\
-																																			\
+#define DEFINE_BINARY_NODE_METHODS(type)																												\
+enum binary_node_status create_binary_##type##_node(struct binary_##type##_node **p_node) {																\
+	*p_node = malloc(sizeof(struct binary_##type##_node));																								\
+	struct binary_##type##_node *const node = *p_node;																									\
+																																						\
+	if (!node)																																			\
+		return BINARY_NODE_STATUS_MALLOC;																												\
+																																						\
+	node->left = NULL;																																	\
+	node->right = NULL;																																	\
+	memset(&(node->data), 0, sizeof(type)); /* NOLINT(clang-analyzer-security.insecureAPI.DeprecatedOrUnsafeBufferHandling) */							\
+																																						\
+	return BINARY_NODE_STATUS_NO_ERROR;																													\
+}																																						\
+																																						\
+enum binary_node_status destroy_binary_##type##_tree(struct binary_##type##_node *p_first_node) { /* This will require post-order traversal.*/			\
+	return BINARY_NODE_STATUS_MALLOC;																													\
+}																																						\
+																																						\
 
